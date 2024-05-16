e6db45c2b9fff3abe48a3becdae7a1de3323f7fa
dnwlwlq123 <dnwlwlq123@naver.com>
2024-05-16 12:51:15 +0900

    jenkins_1

---
+++ src/main/front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP Test... 그리고 이상호</h1>
+      <img src='./image/zz.jpg' alt='히피1' style={{width: '700px', height:'350px'}}></img>
+    </div>
+    // 223.130.143.252(jenkins)
+    // 223.130.131.247(spring boot)
+  );
+};
+
+export default App;
